c352a936e23c1d5186a542ac22bf3f1e1f89a803
JoelmaRegina <joelma_silvaa@hotmail.com>
2021-11-17 01:36:42 -0300

    Rendering the products with JavaScript in the computadores.js

---
+++ README.md
@@ -0,0 +1,12 @@
+# :computer: Front-End && SENAI :woman_student:
+
+#### Copy of the project from my repository [Senai-FrontEnd](https://github.com/joelmaregina/Senai-FrontEnd) to use in the Git Hub pages.
+
+#### _Cópia do projeto do meu repositório  ["Senai-FrontEnd"](https://github.com/joelmaregina/Senai-FrontEnd) para uso do GitHub Pages_ .
+
+##### You can check the project (still under construction) through the link: [Game Mania](https://joelmaregina.github.io/game-mania/index.html)
+
+##### _Você pode conferir o projeto (ainda em construção) através do link:  [Game Mania](https://joelmaregina.github.io/game-mania/index.html)_
+
+
+
---
+++ js/computadores.js
@@ -0,0 +1,81 @@
+
+let listaProdutos = [
+    {
+        imagem: "img/Pc-All-In-One-Black.jpeg",
+        alt: "Foto de um computador All In One Preto",
+        nomeProduto: "Computador All In One Dell",
+        preco: "R$2599,99",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/tablet.jpeg",
+        alt: "Foto de um tablet preto",
+        nomeProduto: "Tablet Infomais Tech ",
+        preco: "R$ 1699,99",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/notebookwhite.jpg",
+        alt: "Foto de um notebook branco",
+        nomeProduto: "Notebook HP Branco",
+        preco: "2199,95",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/tabletWhite.jpeg",
+        alt: "Foto de um tablet branco",
+        nomeProduto: "Tablet Sony Branco",
+        preco: "R$1899,99",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/notebookgrey.jpg",
+        alt: "Foto de um notebook cinza",
+        nomeProduto: "Notebook LG Cinza",
+        preco: "1799,98",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/tablet2.jpeg",
+        alt: "Foto de um tablet preto",
+        nomeProduto: "Tablet AOC preto",
+        preco: "1399,95",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/comptApple.jpg",
+        alt: "Foto de um Computador All In One da Apple",
+        nomeProduto: "Computador All In One Apple",
+        preco: "8799,98",
+        parcelas: "12"
+    },
+    {
+        imagem: "img/notebookdark.jpeg",
+        alt: "Foto de um notebook preto",
+        nomeProduto: "Notebook Positivo Preto",
+        preco: "2799,95",
+        parcelas: "12"
+    }
+]
+
+function renderizarProdutos(){
+    let loja = document.getElementById("espaco-produtos");
+
+    let template = "";
+    
+    for (let i = 0; i < listaProdutos.length; i++) {
+        const produto = listaProdutos[i];
+
+        console.log("ok")
+
+        template += `<div class="produto-loja">
+            <a href="#">
+                <img src="${produto.imagem}" alt="${produto.alt}" class="imagem_compra">
+                <p class="produto_pagina nome-produto text-center"> ${produto.nomeProduto}</p>
+                <p class="preco_pagina preco-produto text-center"> <strong class="price-green"> ${produto.preco}</strong> em até <br> ${produto.parcelas}x sem juros</p>
+            </a>
+        </div>`
+        loja.innerHTML = template;   
+    }
+
+}
